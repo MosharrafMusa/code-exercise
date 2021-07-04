21b96693c174de7fb93c195cba8fa22b3007eddb
Mosharraf Musa <mosharrafny@gmail.com>
2021-07-03 22:30:58 -0400

    counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <h1>welcome from my app!</h1>
-      
+      <Counter/>
     </div>
   );
 }
